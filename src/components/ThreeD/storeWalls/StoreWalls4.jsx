af45078ac05aa0bf08258dfb7f27330dfdc258c1
Aditya <iit2022003@iiitl.ac.in>
2024-10-17 19:11:44 +0530

    updated store walls

--- src/components/ThreeD/storeWalls/StoreWalls4.jsx
+++ src/components/ThreeD/storeWalls/StoreWalls4.jsx
@@ -29,9 +29,9 @@ const StoreWalls4 = () => {
   return (
     <>
       <mesh
-        rotation-y={Math.PI * -0.5}
         position={[-49.8, 9.3, 30]}
         receiveShadow
+        scale={[0.87,1.25,1]}
       >
         <planeGeometry args={[46, 15]} />
         <meshStandardMaterial
