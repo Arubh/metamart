4fb903f7a4bfa85fc3abe9e1891b6652952164b5
Aditya <iit2022003@iiitl.ac.in>
2024-10-17 19:01:37 +0530

    added mesh to wall

--- src/components/ThreeD/storeWalls/StoreWalls.jsx
+++ src/components/ThreeD/storeWalls/StoreWalls.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import * as THREE from "three";
 
 const StoreWalls = () => {
-  // const map = useTexture("./textures/store_walls/beige_wall_001_diff_1k.png");
+  const map = useTexture("./textures/store_walls/beige_wall_001_diff_1k.png");
   const displacementMap = useTexture(
     "./textures/store_walls/beige_wall_001_disp_1k.png"
   );
@@ -30,13 +30,13 @@ const StoreWalls = () => {
     <>
       <mesh position={[0, 9.3, -50.2]} scale={[1,1.25,1]} receiveShadow>
         <planeGeometry args={[100, 15]} />
-        <meshStandardMaterial
+        {/* <meshStandardMaterial
           map={map}
           displacementMap={displacementMap}
           normalMap={normalMap}
           roughnessMap={roughMap}
           side={THREE.DoubleSide}
-        />
+        /> */}
       </mesh>
     </>
   );
