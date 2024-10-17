63f90b384ae5c73848e55d7c491577c08e670eb8
Aditya <iit2022003@iiitl.ac.in>
2024-10-17 19:31:39 +0530

    updated barrier 2

--- src/components/ThreeD/furniture/Barrier2.jsx
+++ src/components/ThreeD/furniture/Barrier2.jsx
@@ -15,6 +15,12 @@ export function Barrier2(props) {
       scale={2}
       rotation={[0, Math.PI / 2, 0]}
     >
+    <mesh
+      castShadow
+      receiveShadow
+      geometry={nodes.Cube.geometry}
+      material={materials.Barrier}
+    />
       <mesh
         castShadow
         receiveShadow
