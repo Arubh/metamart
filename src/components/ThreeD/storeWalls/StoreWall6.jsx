0d7305376c36bb8744686827b06653eb74cee648
Aditya <iit2022003@iiitl.ac.in>
2024-10-17 19:07:27 +0530

    updated store walls

--- src/components/ThreeD/storeWalls/StoreWall6.jsx
+++ src/components/ThreeD/storeWalls/StoreWall6.jsx
@@ -30,7 +30,7 @@ const StoreWalls6 = () => {
     <>
       <mesh
         rotation-y={Math.PI * -0.5}
-        // position={[-49.8, 16.6, 0.58]}
+        position={[-49.8, 16.6, 0.58]}
         scale={[2.35, 1.8, 1]}
         receiveShadow
       >
